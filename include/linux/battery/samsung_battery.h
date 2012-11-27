d2bf0f0480b74a8cb00a8c3c91edf646c1aa9edd
Janson Kang <temasek71@gmail.com>
2012-11-27 11:29:08 +0800

    Fix derp - samsung_battery.h

--- include/linux/battery/samsung_battery.h
+++ include/linux/battery/samsung_battery.h
@@ -115,6 +115,7 @@ struct battery_info {
 	unsigned int cable_type;
 	unsigned int cable_sub_type;
 	unsigned int cable_pwr_type;
+	int online_prop;
 
 	/* For SAMSUNG charge spec */
 	unsigned int vf_state;
@@ -175,9 +176,7 @@ struct battery_info {
 	bool errortest_stopcharging;
 #endif
 
-#if defined(CONFIG_TARGET_LOCALE_KOR) || defined(CONFIG_MACH_M0_CTC)
-	bool is_unspec_phase;
-	bool is_unspec_recovery;
+	/* previous state */
 	unsigned int prev_cable_type;
 	unsigned int prev_battery_health;
 	unsigned int prev_charge_virt_state;
@@ -198,9 +197,7 @@ extern bool is_jig_attached;
 #endif
 
 /* charger detect source */
-#if defined(CONFIG_MACH_C1_KOR_SKT) || \
-	defined(CONFIG_MACH_C1_KOR_KT) || defined(CONFIG_MACH_C1_KOR_LGT) || \
-	defined(CONFIG_MACH_BAFFIN)
+#if defined(CONFIG_MACH_BAFFIN)
 #undef USE_CHGIN_INTR
 #else
 #define USE_CHGIN_INTR
@@ -212,6 +209,12 @@ extern bool is_jig_attached;
 #else
 #undef EXTENDED_ONLINE_TYPE
 #endif
+
+enum online_property {
+	ONLINE_PROP_UNKNOWN = 0,
+	ONLINE_PROP_AC,
+	ONLINE_PROP_USB,
+};
 
 /* use 2step charge termination */
 #if defined(CONFIG_MACH_T0)
