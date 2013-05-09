fdc6fec5101b17207eb9dc0fdfd20234eb16ddf2
Janson Kang <temasek71@gmail.com>
2013-05-09 16:06:28 +0800

    Revert "sound/soc/codecs/wm8994.c: Fix compile error"
    This reverts commit acdc9229aeb9491bf55e82c164b987f877842f44.

--- sound/soc/codecs/wm8994.c
+++ sound/soc/codecs/wm8994.c
@@ -199,13 +199,7 @@ int wm8994_volatile(struct snd_soc_codec *codec, unsigned int reg)
 	}
 }
 
-#ifdef CONFIG_SND_VOODOO
-#include "wm8994_voodoo.h"
-#endif
-
-#ifdef CONFIG_SND_BOEFFLA
 #include "boeffla_sound.h"
-#endif
 
 #ifdef CONFIG_SND_WOLFSON_SOUND_CONTROL
 #include "sound_control.h"
@@ -218,13 +212,7 @@ static int wm8994_write(struct snd_soc_codec *codec, unsigned int reg,
 
 	BUG_ON(reg > WM8994_MAX_REGISTER);
 
-#ifdef CONFIG_SND_VOODOO
-	value = voodoo_hook_wm8994_write(codec, reg, value);
-#endif
-
-#ifdef CONFIG_SND_BOEFFLA
 	value = Boeffla_sound_hook_wm8994_write(reg, value);
-#endif
 
 #ifdef CONFIG_SND_WOLFSON_SOUND_CONTROL
 	value = sound_control_hook_wm8994_write(reg, value);
@@ -597,7 +585,7 @@ static int wm8994_get_retune_mobile_enum(struct snd_kcontrol *kcontrol,
 	return 0;
 }
 
-#if defined(CONFIG_MACH_C1_KOR_LGT) || defined(CONFIG_MACH_BAFFIN_KOR_LGT)
+#if defined(CONFIG_MACH_C1_KOR_LGT)
 static const char *fm_control[] = {
 "OFF", "RCV", "EAR", "SPK", "SPK",
 };
@@ -846,12 +834,10 @@ SOC_ENUM("AIF2DAC Noise Gate Hold Time", wm8958_aif2dac_ng_hold),
 SOC_SINGLE_TLV("AIF2DAC Noise Gate Threshold Volume",
 	       WM8958_AIF2_DAC_NOISE_GATE, WM8958_AIF2DAC_NG_THR_SHIFT,
 	       7, 1, ng_tlv),
-#if defined(CONFIG_MACH_C1_KOR_LGT) || defined(CONFIG_MACH_BAFFIN_KOR_LGT)
+#if defined(CONFIG_MACH_C1_KOR_LGT)
 SOC_ENUM_EXT("FM Control", fm_control_enum,
 		wm8994_get_fm_control, wm8994_put_fm_control),
 #endif
-SOC_SINGLE("AIF2ADCL DAT Invert", WM8994_AIF2ADC_DATA, 1, 1, 0),
-SOC_SINGLE("AIF2ADCR DAT Invert", WM8994_AIF2ADC_DATA, 0, 1, 0),
 };
 
 static const struct snd_kcontrol_new wm1811_snd_controls[] = {
@@ -1299,11 +1285,9 @@ static int aif2clk_ev(struct snd_soc_dapm_widget *w,
 		snd_soc_update_bits(codec, WM8994_POWER_MANAGEMENT_5,
 				    WM8994_AIF2DACL_ENA |
 				    WM8994_AIF2DACR_ENA, 0);
- #if !defined(CONFIG_MACH_BAFFIN)
 		snd_soc_update_bits(codec, WM8994_POWER_MANAGEMENT_4,
 				    WM8994_AIF2ADCL_ENA |
 				    WM8994_AIF2ADCR_ENA, 0);
- #endif
 
 		val = snd_soc_read(codec, WM8994_CLOCKING_1);
 		if (val & WM8994_AIF1DSPCLK_ENA)
@@ -2803,7 +2787,7 @@ static int wm8994_hw_params(struct snd_pcm_substream *substream,
 		return -EINVAL;
 	}
 
-	bclk_rate = params_rate(params) * 2;
+	bclk_rate = params_rate(params) * 4;
 	switch (params_format(params)) {
 	case SNDRV_PCM_FORMAT_S16_LE:
 		bclk_rate *= 16;
@@ -3160,9 +3144,6 @@ static int wm8994_codec_resume(struct snd_soc_codec *codec)
 		/* force a HW read */
 		val = wm8994_reg_read(codec->control_data,
 				      WM8994_POWER_MANAGEMENT_5);
-		if (val < 0)
-			dev_err(codec->dev, "[%s]Failed to read PM5 ret[%d]\n",
-					__func__, val);
 
 		/* modify the cache only */
 		codec->cache_only = 1;
@@ -4254,9 +4235,6 @@ static int wm8994_codec_probe(struct snd_soc_codec *codec)
 					ARRAY_SIZE(wm8958_intercon));
 		break;
 	}
-#ifdef CONFIG_SND_VOODOO
-	voodoo_hook_wm8994_pcm_probe(codec);
-#endif
 
 #ifdef CONFIG_SND_BOEFFLA
 	Boeffla_sound_hook_wm8994_pcm_probe(codec);
